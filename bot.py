cced72d3be4677ff77c8e3c6489dd74aba45ddb8
Agustin Genoves <agustingenoves@hotmail..com>
2024-12-22 15:44:19 -0300

    Commit inicial

---
+++ bot.py
@@ -0,0 +1,15 @@
+import os
+import requests
+
+CHAT_ID = "254021544"
+TELEGRAM_API_TOKEN = os.environ.get("TELEGRAM_API_TOKEN")
+
+
+def send_telegram_message(message: str):
+    url = f"https://api.telegram.org/bot{TELEGRAM_API_TOKEN}/sendMessage"
+    response = requests.post(url, data={"chat_id": CHAT_ID, "text": message})
+    if response.status_code == 200:
+        return True
+    else:
+        return False
+        # print(f"Error al enviar el mensaje: {response.status_code}, {response.text}")
---
+++ main.py
@@ -0,0 +1,27 @@
+import requests
+from bs4 import BeautifulSoup
+from bot import send_telegram_message
+
+url = "https://www.cgeonline.com.ar/informacion/apertura-de-citas.html"
+
+response = requests.get(url)
+
+if response.status_code == 200:
+    soup = BeautifulSoup(response.text, "html.parser")
+
+    rows = soup.find_all("tr")
+    for row in rows:
+
+        columns = row.find_all("td")
+        if (
+            columns
+            and "pasaportesrenovación" in columns[0].get_text(strip=True).lower()
+        ):
+            third_col = columns[2].get_text(strip=True)
+            if third_col == "fecha por confirmar":
+                send_telegram_message(
+                    "Aun no hay fechas para solicitar turno para pasaporte"
+                )
+            break
+else:
+    print(f"Error al acceder a la página: {response.status_code}")
