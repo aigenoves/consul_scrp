801d389d4c52236893220fe51c647fc1f89e02a8
Agustin Genoves <agustingenoves@hotmail..com>
2024-12-22 19:03:46 -0300

    Prueba9

--- bot.py
+++ bot.py
@@ -1,15 +1,18 @@
 import os
 import requests
 
-CHAT_ID = "254021544"
+CHAT_IDS = os.environ.get("TELEGRAM_CHAT_IDS").SPLIT(",")
 TELEGRAM_API_TOKEN = os.environ.get("TELEGRAM_API_TOKEN")
 
 
 def send_telegram_message(message: str):
     url = f"https://api.telegram.org/bot{TELEGRAM_API_TOKEN}/sendMessage"
-    response = requests.post(url, data={"chat_id": CHAT_ID, "text": message})
-    if response.status_code == 200:
-        return True
-    else:
-        print(f"Error al enviar el mensaje: {response.status_code}, {response.text}")
-        return False
+    for chat_id in CHAT_IDS:
+        response = requests.post(url, data={"chat_id": chat_id, "text": message})
+        if response.status_code == 200:
+            return True
+        else:
+            print(
+                f"Error al enviar el mensaje: {response.status_code}, {response.text}"
+            )
+            return False
